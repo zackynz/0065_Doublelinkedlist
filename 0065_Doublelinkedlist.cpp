d453963e56f8d1cf7503aa8592223b816ba29fc0
zackynz <nurasfanz@gmail.com>
2024-05-05 00:40:18 +0700

    menguji true or false pada current dan previous

--- 0065_Doublelinkedlist.cpp
+++ 0065_Doublelinkedlist.cpp
@@ -37,3 +37,61 @@ void addNode() {
         Node* current = START; 
         Node* previous = NULL; 
         while (current != NULL && current->noMhs < newNode->noMhs)
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int noMhs;
+    string name;
+    Node* next;
+    Node* prev;
+};
+
+Node* START = NULL;
+
+void addNode();
+ {
+    Node* newNode = new Node(); // step 1: create a new node
+    cout << "\nEnter the roll number of the student: ";
+    cin >> newNode->noMhs; // Assign value to the data field of the new node
+    cout << "\nEnter the name of the student : ";
+    cin >> newNode->name; // Assign velue to the data field of the new node
+
+    if (START == NULL || newNode->noMhs <= START->noMhs)
+    { 
+        if (START != NULL && newNode->noMhs == START->noMhs)
+        {
+            cout << "\033[31mDuplicate roll numbers allowed\033[0m" << endl;
+            return;
+        }
+        newNode->next = START; 
+        if (START != NULL)
+        {
+            START->prev = newNode; 
+        }
+        newNode->prev = NULL; 
+        START = newNode; 
+    }
+    else
+    {
+        Node* current = START; 
+        Node* previous = NULL; 
+        while (current != NULL && current->noMhs < newNode->noMhs)
+        { 
+            previous = current; 
+            current = current->next; 
+        }
+
+        newNode->next = current; 
+        newNode->prev = previous; 
+
+        if (current != NULL)
+        {
+            current->prev = newNode;
+        }
+        if (previous != NULL)
+        {
+            previous->next = newNode; 
+        }
+
+
