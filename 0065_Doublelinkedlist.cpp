5f53eec5ed73f8d8d89f51d0632e2205ced96845
zackynz <nurasfanz@gmail.com>
2024-05-05 00:47:51 +0700

    memberikan perintah return untuk menghentikan kode

--- 0065_Doublelinkedlist.cpp
+++ 0065_Doublelinkedlist.cpp
@@ -64,4 +64,9 @@ bool search(int rollNo, Node** previous, Node** current) {
     *previous = NULL;
     *current = START;
     while (*current != NULL && (*current)->noMhs != rollNo)
-   
+    {
+        *previous = *current;
+        *current = (*current)->next;
+    }
+    return (*current != NULL);
+}
