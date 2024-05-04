06de77b12538ae10c827a50abbb946bcdba15ba6
zackynz <nurasfanz@gmail.com>
2024-05-05 00:28:05 +0700

    memunculkan nomor

--- 0065_Doublelinkedlist.cpp
+++ 0065_Doublelinkedlist.cpp
@@ -20,5 +20,12 @@ void addNode() {
 
      if (START == NULL || newNode->noMhs <= START->noMhs)
      {
-        
+          if (START != NULL && newNode->noMhs == START->noMhs)
+         {
+             cout << "\033[31mDuplicate roll numbers allowed\033[0m" << endl;
+            return;
+        }
      }
+         
+         
+     
