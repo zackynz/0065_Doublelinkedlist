b522fc57f049956b90ed7a687d21e23ece75d7e4
zackynz <nurasfanz@gmail.com>
2024-05-05 00:25:12 +0700

    memberikan perintah void

--- 0065_Doublelinkedlist.cpp
+++ 0065_Doublelinkedlist.cpp
@@ -8,3 +8,12 @@ struct Node
     Node* next;
     Node* prev;
 };
+
+Node* START = NULL;
+
+void addNode() {
+    Node* newNode = new Node(); // step 1: create a new node
+    cout << "\nEnter the roll number of the student: ";
+    cin >> newNode->noMhs; // Assign value to the data field of the new node
+    cout << "\nEnter the name of the student : ";
+    cin >> newNode->name; // Assign velue to the data field of the new node
